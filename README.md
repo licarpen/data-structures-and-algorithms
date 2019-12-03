d57e7a7058f31529848a236862ccde8e0cfc8247
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-02 17:11:44 -0800

    passing challenge

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # data-structures-and-algorithms
+
+# Table of Contents
+
+* Challenges 
+    * Array-reverse
---
+++ challenges/arrayReverse/array-reverse.js
@@ -0,0 +1,25 @@
+const reverseArray = arr => {
+    return arr.map((val, i) => arr[arr.length - i - 1]
+    );
+};
+
+const reverseArray2 = arr => {
+    const len = arr.length;
+    const reversedArr = [];
+    for(let i = 0; i < len; i++){
+        reversedArr[len - i - 1] = arr[i];
+    }
+    return reversedArr;
+};
+
+const reverseArray3 = arr => {
+    const reversedArr = [];
+    arr.forEach(element => {
+        reversedArr.unshift(element);
+    });
+    return reversedArr;
+}
+
+console.log(reverseArray([1, 2, 3]));
+console.log(reverseArray2([1, 2, 3]));
+console.log(reverseArray3([1, 2, 3]));
