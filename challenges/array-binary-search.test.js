0a969d2a4c4bcd53a06d9c369141004282411ae7
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-04 21:49:28 -0800

    passing binary search tests

--- challenges/array-binary-search.test.js
+++ challenges/array-binary-search.test.js
@@ -3,10 +3,10 @@ const { binarySearch } = require('./array-binary-search');
 describe('binary search algorithm', () => {
   describe('array shift', () => {
     it('returns the index of the key when the key is present', () => {
-      expect(binarySearch([1, 2, 4, 5, 10], 5)).toEqual(3);
+      expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8)).toEqual(7);
     });
     it('returns -1 when the key is not present', () => {
-      expect(binarySearch([1, 2, 4, 5, 10], 3)).toEqual(-1);
+      expect(binarySearch([1, 2, 4, 5, 6], 3)).toEqual(-1);
     });
   });
 });
