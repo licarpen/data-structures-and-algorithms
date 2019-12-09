8ed8f6e3c03e2758ba54cee81af5697fbdf90fe1
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-09 10:24:16 -0800

    linked list head points to first node successfully

--- challenges/linkedList/LinkedList.test.js
+++ challenges/linkedList/LinkedList.test.js
@@ -10,11 +10,14 @@ describe('LinkedList Class', () => {
   it('properly inserts into the linked list', () => {
     linkedList.insert(0);
     expect(linkedList.length).toEqual(1);
+    
   });
   it('is such that the head property will properly point to the first node in the linked list', () => {
-    expect(insertShiftArray([1, 2, 4, 5], 3)).toEqual([1, 2, 3, 4, 5]);
+    expect(linkedList.head).toEqual({ 'next': null, 'value': 0 });
   });
   it('properly inserts multiple nodes into the linked list', () => {
+    linkedList.insert(1);
+    linkedList.insert(2);
     expect(insertShiftArray([1, 2, 4, 5], 3)).toEqual([1, 2, 3, 4, 5]);
   });
   it('returns true when finding a value within the linked list that exists', () => {
