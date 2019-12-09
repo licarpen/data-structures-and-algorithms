ee1f16f70469b6a8848b664d86e7908be28e01c3
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-09 12:07:03 -0800

    passing all tests

--- challenges/linkedList/LinkedList.js
+++ challenges/linkedList/LinkedList.js
@@ -8,43 +8,40 @@ class Node {
 class LinkedList {
   constructor(){
     this.head = null;
-    this.length = 0;
   }
 
   insert(value){
-    // create a node with node value and next pointer of null
     const node = new Node(value);
     if(this.head === null){
       this.head = node;
     }
     else {
-      this.next = this.head;
+      node.next = this.head;
       this.head = node;
     }
-    this.length ++; 
   }
 
   includes(value){
-    let current = this.head;
-    if(current.value === value){
+    let currentNode = this.head;
+    if(currentNode.value === value){
       return true;
     }
-    while(current.next !== null){
-      if(current.value === value){
+    while(currentNode.next !== null){
+      if(currentNode.value === value){
         return true;
       }
-      current = current.next;
+      currentNode = currentNode.next;
     }
     return false;
   }
 
   toString(){
-    let str;
-    let current = this.head;
-    str += current.value;
-    while(current.next !== null){
-      current.value = current.next;
-      str += ` -> ${current.value}`;
+    let str = '';
+    let currentNode = this.head;
+    str += currentNode.value;
+    while(currentNode.next !== null){
+      currentNode = currentNode.next;
+      str += ` -> ${currentNode.value.toString()}`;
     }
     return str;
   }
--- challenges/linkedList/LinkedList.test.js
+++ challenges/linkedList/LinkedList.test.js
@@ -1,6 +1,4 @@
-const { LinkedList, Node } = require('./LinkedList');
-
-
+const { LinkedList } = require('./LinkedList');
 
 describe('LinkedList Class', () => {
   const linkedList = new LinkedList();
@@ -9,8 +7,7 @@ describe('LinkedList Class', () => {
   });
   it('properly inserts into the linked list', () => {
     linkedList.insert(0);
-    expect(linkedList.length).toEqual(1);
-    
+    expect(linkedList.head).toEqual({ 'next': null, 'value': 0 }); 
   });
   it('is such that the head property will properly point to the first node in the linked list', () => {
     expect(linkedList.head).toEqual({ 'next': null, 'value': 0 });
@@ -18,15 +15,20 @@ describe('LinkedList Class', () => {
   it('properly inserts multiple nodes into the linked list', () => {
     linkedList.insert(1);
     linkedList.insert(2);
-    expect(linkedList.next).toEqual({ 'next': null, 'value': 1 });
+    let nextNode = linkedList.head.next;
+    nextNode = nextNode.next;
+    expect(nextNode.value).toEqual(0);
   });
   it('returns true when finding a value within the linked list that exists', () => {
-    expect(linkedList.includes(2)).toBeTruthy();
+    expect(linkedList.includes(1)).toBeTruthy();
   });
   it('returns false when searching for a value in the linked list that does not exist', () => {
     expect(linkedList.includes(4)).toBeFalsy();
   });
   it('properly returns a collection of all the values that exist in the linked list', () => {
-    expect(insertShiftArray([1, 2, 4, 5], 3)).toEqual([1, 2, 3, 4, 5]);
+    linkedList.insert(3);
+    linkedList.insert(4);
+    linkedList.insert(5);
+    expect(linkedList.toString()).toEqual('5 -> 4 -> 3 -> 2 -> 1 -> 0');
   });
 });
