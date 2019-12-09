71e9b80267fc4249f191ca59c3d813c81fd0e7e7
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-09 13:27:23 -0800

    fixed travis issues

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -1,25 +1,24 @@
 const reverseArray = arr => {
-    return arr.map((val, i) => arr[arr.length - i - 1]
-    );
+  return arr.map((val, i) => arr[arr.length - i - 1]
+  );
 };
 
 const reverseArray2 = arr => {
-    const len = arr.length;
-    const reversedArr = [];
-    for(let i = 0; i < len; i++){
-        reversedArr[len - i - 1] = arr[i];
-    }
-    return reversedArr;
+  const len = arr.length;
+  const reversedArr = [];
+  for(let i = 0; i < len; i++){
+    reversedArr[len - i - 1] = arr[i];
+  }
+  return reversedArr;
 };
 
 const reverseArray3 = arr => {
-    const reversedArr = [];
-    arr.forEach(element => {
-        reversedArr.unshift(element);
-    });
-    return reversedArr;
+  const reversedArr = [];
+  arr.forEach(element => {
+    reversedArr.unshift(element);
+  });
+  return reversedArr;
 };
 
-console.log(reverseArray([1, 2, 3]));
-console.log(reverseArray2([1, 2, 3]));
-console.log(reverseArray3([1, 2, 3]));
+module.exports({ reverseArray, reverseArray2, reverseArray3 });
+
