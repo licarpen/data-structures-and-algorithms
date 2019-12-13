3486ce744da27be13c7bd95771264bf140aa2605
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-12 21:24:04 -0800

    getting closer to a working, elegent solutionn to linked list merge

--- challenges/linkedList/mergeLinkedLists.js
+++ challenges/linkedList/mergeLinkedLists.js
@@ -1,13 +1,19 @@
-const { LinkedList, Node } = require('./LinkedList');
+const { LinkedList } = require('./LinkedList');
 
 const mergeLinkedLists = (list1, list2) => {
-  const list3 = new LinkedList();
   let currentNode1 = list1.head;
   let currentNode2 = list2.head;
-
-  
-
-
+  while(currentNode1.next || currentNode2.next){
+    if(currentNode2.next){
+      currentNode1.next = currentNode2;
+      currentNode2 = currentNode2.next;
+    }
+    if(currentNode1.next){
+      currentNode2.next = currentNode1;
+      currentNode1 = currentNode1.next;
+    }
+  }
+  return list1.head;
 };
 
 const linkedList1 = new LinkedList();
@@ -20,4 +26,4 @@ linkedList2.insert(1);
 linkedList2.insert(3);
 linkedList2.insert(5);
 
-mergeLinkedLists(linkedList1, linkedList2);
+console.log(mergeLinkedLists(linkedList1, linkedList2));
