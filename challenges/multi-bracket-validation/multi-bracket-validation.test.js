8cbd7c8b747ca19a2d69308ea88663cd04407741
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-18 12:52:33 -0800

    wrote tests for bracket validation

---
+++ challenges/multi-bracket-validation/multi-bracket-validation.test.js
@@ -0,0 +1,22 @@
+const validateBrackets = require('./multi-bracket-validation');
+
+describe('Multi-Bracket Validation', () => {
+  it('validates a string containing a valid rounded brackets pair', () => {
+    expect(validateBrackets('(Hello, world!)')).toBeTruthy();
+  });
+  it('validates a string containing a valid square brackets pair', () => {
+    expect(validateBrackets('[]')).toBeTruthy();
+  });
+  it('validates a string containing a valid curly brackets pair', () => {
+    expect(validateBrackets('{}')).toBeTruthy();
+  });
+  it('validates a string containing nested brackets', () => {
+    expect(validateBrackets('([{}])')).toBeTruthy();
+  });
+  it('rejects a string containing an incomplete brackets pair', () => {
+    expect(validateBrackets('({}')).toBeFalsy();
+  });
+  it('rejects a string containing invalid brackets', () => {
+    expect(validateBrackets('{[}]')).toBeFalsy();
+  });
+});
