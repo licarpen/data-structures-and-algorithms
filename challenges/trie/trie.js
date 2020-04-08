5c59e8b787e325531b8915d3436bf123cfa10840
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-04-08 15:49:46 -0700

    add trie data structure and update readme

---
+++ challenges/trie/trie.js
@@ -0,0 +1,107 @@
+// Trie
+// Given a dictionary stored in a trie and a prefix, list and count the words with given prefix.
+
+
+class Node {
+
+  constructor(data){
+    this.value = data;
+    this.children = [];
+    this.isEnd = false;
+  }
+}
+
+class Trie {
+
+  constructor(){
+    this.root = new Node(null);
+  }
+
+  add(word){
+    
+    let current = this.root;
+    for(const char of word) {
+      let found = false;
+      for(let i = 0; i < current.children.length; i++){
+        if(current.children[i].value === char) {
+          current = current.children[i];
+          found = true;
+          break;
+        }
+      }
+      if(!found){
+        const node = new Node(char);
+        current.children.push(node);
+        current = node;
+      }
+    }          
+    current.isEnd = true;
+  }
+
+  contains(word){
+    let current = this.root;
+    for(const char of word) {
+      let found = false;
+      for(let i = 0; i < current.children.length; i++){
+        if(current.children[i].value === char) {
+          current = current.children[i];
+          found = true;
+          break;
+        }
+      }
+      if(!found) return false;
+    }
+    return current.isEnd;
+  }
+
+  countWordsWithPrefix(pre){
+    let current = this.root;
+    let found;
+    const result = {
+      count: 0,
+      words: []
+    };
+
+    for(const char of pre){
+      found = false;
+      for(let i = 0; i < current.children.length; i++){
+        if(current.children[i].value === char) {
+          current = current.children[i];
+          found = true;
+          break;
+        }
+      }
+    }
+    if(!found) return result;
+
+    const countWords = (current, result, word = pre) => {
+      if(current.isEnd){
+        result.words.push(word);
+        result.count++;
+      }
+      if(current.children.length > 0){
+        current.children.forEach(node => {
+          word = word + node.value;
+          result = countWords(node, result, word);
+        });
+      }
+      return result;
+    };
+    
+    return countWords(current, result);
+  }
+  
+  print() {
+    console.log(JSON.stringify(this, null, 2));
+  }
+}
+
+const aTrie = new Trie();
+aTrie.add('apk');
+aTrie.add('bat');
+aTrie.add('apko');
+aTrie.add('ba');
+aTrie.add('bate');
+aTrie.contains('b');
+// aTrie.countWordsWithPrefix('a');
+console.log(aTrie.countWordsWithPrefix('bf'));
