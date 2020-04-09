73601ce6d19dcaa811e6d21a04d9cedb34e823af
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-04-08 19:48:02 -0700

    add tests for Trie methods add() and contains()

--- challenges/trie/trie.test.js
+++ challenges/trie/trie.test.js
@@ -12,4 +12,28 @@ describe('Trie class', () => {
     trie.add('a');
     expect(trie.print()).toEqual({ root: { value: null, children: [{ value: 'a', children: [], isEnd: true }], isEnd: false } }); 
   });
+  it('properly adds a string to a trie', () => {
+    trie.add('ba');
+    expect(trie.print()).toEqual({ 'root': { 'children': [{ 'children': [{ 'children': [], 'isEnd': true, 'value': 'a' }], 'isEnd': false, 'value': 'b' }], 'isEnd':
+    false, 'value': null } });
+  });
+  it('properly adds a string with characters already existing in the trie', () => {
+    trie.add('ba');
+    trie.add('bat');
+    expect(trie.print()).toEqual({ 'root': { 'children': [{ 'children': [{ 'children': [{ 'children': [], 'isEnd': true, 'value': 't' }], 'isEnd': true, 'value': 'a' }], 'isEnd': false, 'value': 'b' }], 'isEnd': false, 'value': null } }); 
+  });
+  it('properly adds a string with characters not already in the trie', () => {
+    trie.add('bat');
+    trie.add('cat');
+    expect(trie.print()).toEqual({ 'root': { 'children': [{ 'children': [{ 'children': [{ 'children': [], 'isEnd': true, 'value': 't' }], 'isEnd': false, 'value': 'a' }], 'isEnd': false, 'value': 'b' }, { 'children': [{ 'children': [{ 'children': [], 'isEnd': true, 'value': 't' }], 'isEnd': false, 'value': 'a' }], 'isEnd': false, 'value': 'c' }], 'isEnd': false, 'value': null } }); 
+  });
+  it('properly identifies a string already in the trie', () => {
+    trie.add('cat');
+    expect(trie.contains('cat')).toEqual(true); 
+  });
+  it('properly identifies a string not in the trie', () => {
+    trie.add('cat');
+    trie.contains('ca');
+    expect(trie.contains('ca')).toEqual(false); 
+  });
 });
